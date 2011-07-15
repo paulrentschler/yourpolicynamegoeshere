bf4d2bccc7dba556e5841264cd968433e58cbbf2
Brian Davis <bjd18@psu.edu>
2011-07-15 17:06:28 -0400

    Allowed transform.sh to create a new folder instead of overwriting the skeleton

--- transform.sh
+++ transform.sh
@@ -9,10 +9,10 @@ fi
 SHORTNAME=$1
 FULLNAME=$2
 
+# rename the file and folder objects
+cp -rf policy.yourpolicynamegoeshere policy.$SHORTNAME
+mv policy.$SHORTNAME/policy/yourpolicynamegoeshere policy.$SHORTNAME/policy/$SHORTNAME
+
 # insert the policy name where needed
 SEDEXPRESSION='s/yourpolicynamegoeshere/'$SHORTNAME'/g;s/Your Policy Name Goes Here/'$FULLNAME'/g'
-find -X policy.yourpolicynamegoeshere -type f | xargs -I {} sed -i '' -e "$SEDEXPRESSION" {} 
-
-# rename the file and folder objects
-mv policy.yourpolicynamegoeshere/policy/yourpolicynamegoeshere policy.yourpolicynamegoeshere/policy/$SHORTNAME 
-mv policy.yourpolicynamegoeshere policy.$SHORTNAME
+find -X policy.$SHORTNAME -type f | xargs -I {} sed -i '' -e "$SEDEXPRESSION" {} 
